885843d3e25dd18aef6474f324e614131a7be929
dropdashsstore-ux <dropdashsstore@gmail.com>
2025-08-31 09:53:03 +0200

    Remove confetti import for Vercel build

--- components/ConfettiAnimation.tsx
+++ components/ConfettiAnimation.tsx
@@ -1,10 +1,9 @@
 // components/ConfettiAnimation.tsx
 import { useEffect } from "react";
-import runConfetti from "../animations/confetti.js";
 
 const ConfettiAnimation = () => {
   useEffect(() => {
-    runConfetti(); // call the animation function once on mount
+    // Confetti animation removed
   }, []);
 
   return <div id="confetti-container"></div>; // container for animation
